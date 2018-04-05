454e7b268b3a1846afbc440e896d9d1e982039a8
Nathan Easton <ndragon798@yahoo.com>
2018-04-05 11:32:30 -0400

    Fixed up video a bit. Still not perfect but better

--- SimpleGallery.py
+++ SimpleGallery.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, Markup, send_from_directory
+import cv2
 import os
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -10,12 +11,28 @@ users.sort()
 print(users)
 users.pop(0)
 
+
+
+def getposter(filename):
+	cap = cv2.VideoCapture(filename)
+	count = 0
+	ret,frame = cap.read()
+	print(filename,filename[:-4])
+	cv2.imwrite(filename[:-4]+".jpg", frame)
+	cap.release()
+	cv2.destroyAllWindows()  # destroy all the opened windows
+
+
 @app.route("/<user>")
 def userpage(user=''):
 	img_loc=[]
 	for root,subdirs,files in os.walk('./static/users/'+user):
 		print(files)
 		img_loc=files
+	for i in img_loc:
+		if i[-4:]=='.mp4':
+			getposter('./static/users/'+user+'/'+i)
+
 	return render_template('user.html',img_loc=img_loc,user=user)
 
 @app.route("/")
